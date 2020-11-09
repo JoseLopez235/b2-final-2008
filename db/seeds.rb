d473960083a32f37bb668d255cd72b90ed1af261
Jose Lopez <joselopez11394@gmail.com>
2020-11-09 13:09:10 -0500

    adds seeds to test server

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+hospital = Hospital.create!(name: "Grey Sloan Memorial Hospital")
+doctor = Doctor.create!(name: "Meredith Grey", specialty: "General Surgery", university: "Harvard University", hospital_id: hospital.id)
+patient_1 = doctor.patients.create!(name: "Katie Bryce", age: 24)
+patient_2 = doctor.patients.create!(name: "Denny Duquette", age: 39)
+patient_3 = doctor.patients.create!(name: "Rebecca Pope", age: 32)
+patient_4 = doctor.patients.create!(name: "Zola Shepherd", age: 2)
